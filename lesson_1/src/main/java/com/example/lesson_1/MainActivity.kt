b704f406cdf089ead952b7fadc0a1c1c8ca8f352
takumi-saito <takumi.saitoh.1992@gmai.com>
2021-09-10 00:48:55 +0900

    add lesson 1

---
+++ lesson_1/src/main/java/com/example/lesson_1/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lesson_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
+}
